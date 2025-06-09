4d72ed6ba730a15467e91992c97f25e54d7757a8
kennykenny <gbmkocc1@gmail.com>
2025-06-09 12:38:07 +0100

    Trigger GitHub Actions workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,16 +4,6 @@ variable "aws_region" {
   default     = "us-east-1" # You can override via CLI or tfvars
 }
 
-variable "aws_access_key" {
-  type = string
-  default = "AWS_ACCESS_KEY_ID"  # 👉 Replace in GitHub secrets
-}
-
-variable "aws_secret_key" {
-  type = string
-  default = "AWS_SECRET_ACCESS_KEY" # 👉 Replace in GitHub secrets
-}
-
 variable "key_name" {
   description = "Name of the SSH key pair"
   default     = "newkey"   # 👉 Replace with your real AWS key pair name
